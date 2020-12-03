0c1716aac3be634cf6206398c4db59ca09d18776
Gopalakrishnan <krishnan.gopal34@gmail.com>
2020-12-04 02:39:54 +0530

    floater link swapped

--- src/components/Floater/Floater.jsx
+++ src/components/Floater/Floater.jsx
@@ -6,8 +6,8 @@ const Floater = () => {
     return (
         <section id="floater" className="floater">
             <ul className="floater__list">
-                <li className="floater__item"><a href="mailto:bumup@gmail.com" className="floater__link"><Whatsapp width={{width: "42px"}} /></a></li>
-                <li className="floater__item"><a href="https://api.whatsapp.com/send?phone=917092353742" className="floater__link"><Mail width={{width: "42px"}} /></a></li>
+                <li className="floater__item"><a href="https://api.whatsapp.com/send?phone=917092353742" className="floater__link"><Whatsapp width={{width: "42px"}} /></a></li>
+                <li className="floater__item"><a href="mailto:contact@bumpup.co" className="floater__link"><Mail width={{width: "42px"}} /></a></li>
             </ul>
         </section>
     )
