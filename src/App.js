ac3f052b2f271c6d99dba4f348a7add821c30364
Gopalakrishnan <krishnan.gopal34@gmail.com>
2020-12-20 20:33:49 +0530

    favicon icon updated

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/program" component={program} />
-            <Route path="/contact" component={contactPage} />
+            <Route exact path="/contact" component={contactPage} />
             <Route path="/whybumpup" component={whybumpup} />
 
           </Switch>
