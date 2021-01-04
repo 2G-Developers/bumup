757af8903206e76eef1ed695150f5fbc87b9db5c
Gokul <32118741+Gokul2104@users.noreply.github.com>
2021-01-05 01:13:33 +0530

    scrollautomate

--- src/components/Offer/offer.jsx
+++ src/components/Offer/offer.jsx
@@ -1,39 +1,60 @@
-import React ,{ useState , useEffect  } from 'react'
+import React ,{ useState , useEffect ,  useRef  } from 'react'
 import {BrowserRouter as Router, Link, Route, Switch , useLocation } from 'react-router-dom';
 import BabyReady from './BabyReady';
 import BounceBack from './BounceBack';
 import EverythingLabor from './EverythingLabor';
 import MummaStrong from './MummaStrong';
 
-function ScrollToTop() {
-    const { pathname } = useLocation();
-  
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
-  
-    return null;
-  }
+// const scrollToRef = (ref) => {window.scrollTo(0, ref.current.offsetTop) ;alert(ref.current.offsetTop)  }
+function ComponentDidMount(ref,mydesktop) {
+    
+        useEffect(() => {
+            if (ref.current.offsetTop)
+            {
+                window.scrollTo(0, ref.current.offsetTop)  ;
+            }
+            else{
+                window.scrollTo(0, mydesktop.current.offsetTop) ;
+            }
+             }, []);
 
+ 
+    return ref.current.offsetTop;
+  }
+  
 function Offer({class1='offer-page__card-body',class2='offer-page__card-body',class3='offer-page__card-body', class4='offer-page__card-body'}){
     const myurl = window.location.href
-    ScrollToTop()
+    const mydesktop= useRef('desktop')
+    
+    const mybaby= useRef('baby')
+    const mymumma= useRef('mumma')
+    const mylabor= useRef('labor')
+    const mybounce= useRef('bounce')
+    let myphone;
+
     if (myurl.indexOf("mummastrong")!==-1)
     {
         class2='offer-page__card-body-active'
-    
+        myphone = mymumma;
     }
     else if (myurl.indexOf("everythinglabor")!==-1)
     {
         class3='offer-page__card-body-active'
+        myphone = mylabor;
+
     }        
     else if (myurl.indexOf("bounceback")!==-1)
     {
         class4='offer-page__card-body-active'
+        myphone = mybounce;
+
     }   
    else{
     class1 ='offer-page__card-body-active'
+    myphone = mybaby;
+
    }
+   ComponentDidMount(myphone,mydesktop)
     const [state,setclassstate ] = useState({class1state:class1,class2state:class2,class3state:class3,class4state:class4});
     const defaultclass = 'offer-page__card-body'
     const activeclass = 'offer-page__card-body-active'
@@ -70,7 +91,7 @@ function update_active(i)
                             <h1 style={{fontSize: "3.75rem"}}>What do we offer?</h1>
                         </div>
 
-                        <div className="offer-page__container">
+                        <div className="offer-page__container"  >
 
                             <div className="offer-page__container">
                                 <div className="offer-page__text">
@@ -79,18 +100,18 @@ function update_active(i)
                                 <div className="offer-page__text">
                                     <p>From the time you start planning for a baby to helping you make your pregnancy a fit, healthy and happy one, getting you all set for labor — and not to forget, helping you bounce back to your pre-baby self!</p>
                                 </div>
-                                <div className="offer-page__text">
+                                <div className="offer-page__text" ref={mydesktop}>
                                     <p>What more? All our programs are designed as virtual sessions giving you the flexibility to attend them from the cozy comfort of your homes!</p>
                                 </div>
-                                <div className="offer-page__text">
+                                <div className="offer-page__text" >
                                     <p>Here's what you can choose from--</p>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="offer-page__card">
-                            <Link to="/program" className={state.class1state} onClick={() =>update_active(1)}>
-                                <div className="offer-page__card-subheading">
+                        <div className="offer-page__card" >
+                            <Link to="/program" className={state.class1state} onClick={() =>update_active(1)} >
+                                <div className="offer-page__card-subheading" >
                                     <h3>Baby-ready</h3>
                                 </div>
                                 <div className="offer-page__card-text">
@@ -156,8 +177,8 @@ function update_active(i)
                             </div>
                         </div>
 
-                        <div className="offer-page__card">
-                            <Link to="/program" className={state.class1state} onClick={() =>update_active(1)}>
+                        <div className="offer-page__card"   >
+                            <Link to="/program" className={state.class1state} onClick={() =>update_active(1)}  ref={mybaby}>
                                 <div className="offer-page__card-subheading">
                                     <h3>Baby-ready</h3>
                                 </div>
@@ -167,9 +188,9 @@ function update_active(i)
                             </Link>
                                                             
                         <Switch>
-                            <Route exact path="/program" component={BabyReady} />
+                            <Route exact path="/program" component={BabyReady}/>
                         </Switch>
-                            <Link to="/program/mummastrong" className={state.class2state} onClick={() =>update_active(2)}>
+                            <Link to="/program/mummastrong" className={state.class2state} onClick={() =>update_active(2)}  ref={mymumma}>
                                 <div className="offer-page__card-subheading">
                                     <h3>Mumma-Strong</h3>
                                 </div>
@@ -178,9 +199,9 @@ function update_active(i)
                                 </div>
                             </Link>
                         <Switch>
-                            <Route path="/program/mummastrong" component={MummaStrong} />
+                            <Route path="/program/mummastrong" component={MummaStrong}  />
                         </Switch>
-                            <Link to="/program/everythinglabor" className={state.class3state} onClick={() =>update_active(3)}>
+                            <Link to="/program/everythinglabor" className={state.class3state} onClick={() =>update_active(3)}  ref={mylabor}>
                                 <div className="offer-page__card-subheading">
                                     <h3>Everything-labor</h3>
                                 </div>
@@ -189,9 +210,9 @@ function update_active(i)
                                 </div>
                             </Link>
                         <Switch>
-                            <Route path="/program/everythinglabor" component={EverythingLabor} />
+                            <Route path="/program/everythinglabor" component={EverythingLabor} ref={mybaby}/>
                         </Switch>
-                            <Link to="/program/bounceback" className={state.class4state} onClick={() =>update_active(4)}>
+                            <Link to="/program/bounceback" className={state.class4state} onClick={() =>update_active(4)}  ref={mybounce}>
                                 <div className="offer-page__card-subheading">
                                     <h3>Bounce-Back</h3>
                                 </div>
