51baf58f537956e2ce9834ad67edb64280de3156
Gopalakrishnan <krishnan.gopal34@gmail.com>
2021-01-27 17:06:29 +0530

    added new changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import Carousel from './../components/Carousel/Carousel'
 
-import Founder from '../components/Founder/Founder'
+// import Founder from '../components/Founder/Founder'
+import Skeleton from 'react-loading-skeleton';
 import Mantra from '../components/Mantra/Mantra'
 import OfferSecondary from '../components/Offer/OfferSecondary'
-// import Navbar from '../components/Navbar/Navbar';
 import NavbarMobile from '../components/Navbar/NavbarMobile'
+const Founder = React.lazy(() => import(
+  /* 
+  webpackPrefetch: true
+ */ '../components/Founder/Founder'
+  )
+);
 
 function Home() {
   return (
     <div className="App">
-            {/* <Navbar class1="navbar-list-active"/> */}
             <NavbarMobile />
             <Carousel images="https://res.cloudinary.com/gopal1996/image/upload/v1611745202/Bumpup/Home-Banner_uceivk.webp" altText="Hero" 
                 imagesMobile="https://res.cloudinary.com/gopal1996/image/upload/v1611745355/Bumpup/Mobile-Banner_d8o5sh.webp" />
             <OfferSecondary />
 
             <Mantra />
-            <Founder />
+            <Suspense fallback={<Skeleton count={10} />}>
+              <Founder />
+            </Suspense>
     </div>
   );
 }
