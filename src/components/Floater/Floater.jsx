0311f8462bcb3963cc944ee5ce05da1fb7b470d0
Gopalakrishnan <krishnan.gopal34@gmail.com>
2021-01-27 17:12:37 +0530

    added aria-label

--- src/components/Floater/Floater.jsx
+++ src/components/Floater/Floater.jsx
@@ -6,8 +6,8 @@ const Floater = () => {
     return (
         <section id="floater" className="floater">
             <ul className="floater__list">
-                <li className="floater__item"><a href="https://api.whatsapp.com/send?phone=918248749487" className="floater__link"><Whatsapp width="42px" /></a></li>
-                <li className="floater__item"><a href="tel:+918248749487" className="floater__link"><Call width="42px" height="42px" fill="#fff" strokeWidth="12" /></a></li>
+                <li className="floater__item"><a href="https://api.whatsapp.com/send?phone=918248749487" aria-label="Whatsapp" className="floater__link"><Whatsapp width="42px" /></a></li>
+                <li className="floater__item"><a href="tel:+918248749487" aria-label="Call" className="floater__link"><Call width="42px" height="42px" fill="#fff" strokeWidth="12" /></a></li>
             </ul>
         </section>
     )
